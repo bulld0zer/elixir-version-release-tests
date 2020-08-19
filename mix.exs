88856414d18bd5704f7304069235ba55949a7f97
Aleksandr <aleksandr@heathmont.net>
2020-08-19 18:33:50 +0300

    [version_release] start next development iteration 0.0.5-alpha.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule VersionReleaseTests.MixProject do
   def project do
     [
       app: :version_release_tests,
-      version: "0.0.5",
+      version: "0.0.5-alpha.0",
       elixir: "~> 1.8",
       elixirc_paths: ["lib"],
       deps: deps()
