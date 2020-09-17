23c267cfb00f38ef380ea212e91448a484d85f45
Sasha <aleksandr@heathmont.net>
2020-09-17 10:32:53 +0300

    [version_release] start next development iteration 1.0.9-alpha.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule VersionReleaseTests.MixProject do
   def project do
     [
       app: :version_release_tests,
-      version: "1.0.8",
+      version: "1.0.9-alpha.0",
       elixir: "~> 1.8",
       description: "Tests for version_release",
       package: [
