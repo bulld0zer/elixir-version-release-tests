8f7c4114bce30f75ee59c79dec03135e2113e4d1
Aleksandr <aleksandr@heathmont.net>
2021-06-07 17:08:43 +0300

    [version_release] New version: 1.0.13-alpha.7

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule VersionReleaseTests.MixProject do
   def project do
     [
       app: :version_release_tests,
-      version: "1.0.13-alpha.6",
+      version: "1.0.13-alpha.7",
       elixir: "~> 1.8",
       description: "Tests for version_release",
       package: [
